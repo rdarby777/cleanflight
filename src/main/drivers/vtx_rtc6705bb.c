0afe470451bcd1ee4ef44af5d5d5e364e5c6ce3a
jflyper <shinoda@jaist.ac.jp>
2016-03-05 02:07:18 +0900

    Cleaned up includes for masterConfig_t

--- src/main/drivers/vtx_rtc6705bb.c
+++ src/main/drivers/vtx_rtc6705bb.c
@@ -69,43 +69,32 @@
 //	101 (PA1), 204 (PB4), 205 (PB5)
 //	101 (PA1), 102 (PWM7/PA2), 103 (PWM8/PA3)
 
-// Things for config_master.h
+// Things for including config_master.h
 
 #include "common/color.h"
 #include "common/axis.h"
 #include "common/maths.h"
-#include "common/filter.h"
 
 #include "drivers/sensor.h"
 #include "drivers/accgyro.h"
 #include "drivers/compass.h"
-#include "drivers/system.h"
 #include "drivers/gpio.h"
 #include "drivers/timer.h"
 #include "drivers/pwm_rx.h"
-#include "drivers/serial.h"
 
 #include "io/rc_controls.h"
 
 #include "sensors/sensors.h"
 #include "sensors/gyro.h"
-#include "sensors/compass.h"
 #include "sensors/acceleration.h"
-#include "sensors/barometer.h"
 #include "sensors/boardalignment.h"
 #include "sensors/battery.h"
 
-#include "io/beeper.h"
 #include "io/serial.h"
 #include "io/gimbal.h"
 #include "io/escservo.h"
-#include "io/rc_curves.h"
 #include "io/ledstrip.h"
 #include "io/gps.h"
-
-#include "rx/rx.h"
-
-#include "blackbox/blackbox_io.h"
 
 #include "telemetry/telemetry.h"
 
@@ -115,9 +104,6 @@
 #include "flight/failsafe.h"
 #include "flight/altitudehold.h"
 #include "flight/navigation.h"
-
-#include "config/runtime_config.h"
-#include "config/config.h"
 
 #include "config/config_profile.h"
 #include "config/config_master.h"
